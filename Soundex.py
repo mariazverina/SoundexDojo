85259e53dedf7245dc85f537136f8ee6d8d7158f
Maria Zverina <maria.zverina@corp.badoo.com>
2013-09-20 10:19:03 +0100

    Refactor Handle lowercase letters c, g, j, k, q, s, x, z => 2

--- Soundex.py
+++ Soundex.py
@@ -4,6 +4,7 @@ Created on 20 Sep 2013
 
 @author: mariaz
 '''
+from collections import defaultdict
 
 def soundex(aString):
     """Return the soundex value of a string. This is 4 character string with one alpha and 3 digits.
@@ -24,12 +25,33 @@ def soundex(aString):
     >>> soundex('RB'), soundex('RF'), soundex('RP'), soundex('RV')
     ('R100', 'R100', 'R100', 'R100')
     
+    Two consonants in different groups should be mapped to first letter + 1 number + two zeroes
+    Convert: b, f, p, v => 1
+    >>> soundex('RB'), soundex('RF'), soundex('RP'), soundex('RV')
+    ('R100', 'R100', 'R100', 'R100')
+    
+    We should treat all letters as capitals
+    >>> soundex('rb')
+    'R100'
+    
+    Convert: c, g, j, k, q, s, x, z => 2
+    >>> [soundex('r'+x) for x in "c, g, j, k, q, s, x, z".split(", ")]
+    ['R200', 'R200', 'R200', 'R200', 'R200', 'R200', 'R200', 'R200']
     
 """
-    letterValues = {'A': '',
-                    'B': '1', 'P': '1', 'F': '1', 'V': '1'}
-    coded = [letterValues[c] for c in list(aString[1:])] + list('000')
-    return aString[:1] + ''.join(coded)[:3]
+
+    letterValues = defaultdict(lambda: "", 
+                               {'A': '',
+                                'B': '1', 'P': '1', 'F': '1', 'V': '1',
+                                'C': '2', 'G': '2', 'J': '2', 'K': '2',
+                                'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+                                })
+    
+    aString = aString.upper()
+    head = aString[:1]  # first character is dealt with specially
+    tail = aString[1:]
+    coded = [letterValues[c] for c in list(tail)]
+    return head + ''.join(coded + list('000'))[:3]
     
 
 if __name__ == "__main__":
