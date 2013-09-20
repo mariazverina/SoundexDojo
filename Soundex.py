3d2d9fa2f402db5ec562effda905e175755d84f5
Maria Zverina <maria.zverina@corp.badoo.com>
2013-09-20 10:21:22 +0100

    Convert: d, t => 3

--- Soundex.py
+++ Soundex.py
@@ -38,6 +38,10 @@ def soundex(aString):
     >>> [soundex('r'+x) for x in "c, g, j, k, q, s, x, z".split(", ")]
     ['R200', 'R200', 'R200', 'R200', 'R200', 'R200', 'R200', 'R200']
     
+    Convert: d, t => 3
+    >>> soundex('fd'), soundex('ft')
+    ('F300', 'F300')
+    
 """
 
     letterValues = defaultdict(lambda: "", 
@@ -45,6 +49,7 @@ def soundex(aString):
                                 'B': '1', 'P': '1', 'F': '1', 'V': '1',
                                 'C': '2', 'G': '2', 'J': '2', 'K': '2',
                                 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
+                                'D': '3', 'T': '3',
                                 })
     
     aString = aString.upper()
